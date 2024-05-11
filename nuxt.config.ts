88c4bcc0e428a1231a1d0f5b16947d104b6f4643
Yo <128448571+yo001@users.noreply.github.com>
2024-05-11 13:35:10 +0800

    add head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
   app: {
+    head: {
+      title: 'GE-Tools',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
     baseURL: process.env.NODE_ENV === 'production' ? '/GE-Tools/' : '/',
   },
   eslint: {
